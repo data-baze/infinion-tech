e05048d85ecfe99f9fdd282624f9e7e3e41ee957
data-baze <basseydata@gmail.com>
2025-07-19 09:55:05 +0100

    fix: minor ui fixes

--- src/screens/dashboard/components/CardLoading.tsx
+++ src/screens/dashboard/components/CardLoading.tsx
@@ -1,6 +1,6 @@
 const CardLoading = () => {
   return (
-    <div className="bg-background-secondary rounded-lg p-4 flex flex-col items-center text-center shadow animate-pulse">
+    <div className="bg-background-secondary rounded-lg px-4 py-6 space-y-6  flex flex-col items-center text-center shadow animate-pulse">
       <div className="w-20 h-20 rounded-full animate-pulse bg-gray-600 mb-3" />
       <div className="h-4 w-24 animate-pulse bg-gray-600 rounded mb-2" />
       <div className="h-3 w-32 animate-pulse bg-gray-600 rounded" />
--- src/screens/dashboard/components/UserCard.tsx
+++ src/screens/dashboard/components/UserCard.tsx
@@ -9,12 +9,12 @@ const UserCard = ({ user, onClick }: Props) => {
   return (
     <div
       onClick={onClick}
-      className="cursor-pointer bg-background-secondary hover:bg-background-secondary/45 rounded-lg p-4 flex flex-col items-center text-center shadow"
+      className="cursor-pointer bg-background-secondary hover:bg-background-secondary/45 rounded-lg px-4 py-6 flex flex-col items-center text-center shadow space-y-6"
     >
       <img
         src={user.avatar}
         alt={user.name}
-        className="w-20 h-20 rounded-full object-cover mb-3"
+        className="w-20 h-20 rounded-full object-cover"
       />
       <h3 className="text-lg font-semibold text-white">{user.name}</h3>
       <p className="text-sm text-gray-400">{user.email}</p>
